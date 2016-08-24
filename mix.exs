6c2a7a78da988876aaa547723ad9114afae82aae
Alex Shneyderman <a.shneyderman@gmail.com>
2016-08-24 06:46:07 -0400

    Hex packaging hooking up

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule Barracuda.Mixfile do
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()]
   end
 
   def application do
